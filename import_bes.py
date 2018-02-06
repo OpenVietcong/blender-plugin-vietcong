ddbad2a4bfe1b310a768e8905eca116217529576
Jan Havran <havran.jan@email.cz>
2018-02-06 22:32:56 +0100

    BES: parse file by blocks

--- import_bes.py
+++ import_bes.py
@@ -45,23 +45,74 @@ class BES(object):
         self.vertices = [(0, 0, 0), (5, 0, 0), (2.5, 5, 0)]
         self.faces = [(0, 1, 2)]
 
-    def parse(self, fmt):
+    def unpack(self, fmt, data):
         st_fmt = fmt
         st_len = struct.calcsize(st_fmt)
         st_unpack = struct.Struct(st_fmt).unpack_from
-        data = st_unpack(self.f.read(st_len))
-        return data
+        return st_unpack(data[:st_len])
 
     def read_header(self):
-        data = self.parse("<5s4sI3c")
-        print(data)
+        data = self.f.read(0x10)
+        print(self.unpack("<5s4sI3c", data))
 
     def read_preview(self):
         self.f.read(0x3000)
 
     def read_data(self):
-        (label, size) = self.parse("<II")
-        print("Block {} of size {} bytes".format(hex(label),size))
+        data = self.f.read()
+        self.parse_data(data)
+
+    def parse_data(self, data):
+        start = 0
+        while(1):
+            (label, size) = self.unpack("<II", data[start:])
+            print("Block {} of size {} bytes".format(hex(label),size))
+            subblock = data[start+8:size]
+            start += size 
+
+            if   label == 0x001:
+                self.parse_block_mesh(subblock)
+            elif label == 0x030:
+                self.parse_block_unk30(subblock)
+            elif label == 0x031:
+                self.parse_block_unk31(subblock)
+            elif label == 0x032:
+                self.parse_block_vertices(subblock)
+            elif label == 0x033:
+                self.parse_block_faces(subblock)
+            elif label == 0x034:
+                self.parse_block_properties(subblock)
+            elif label == 0x035:
+                self.parse_block_unk35(subblock)
+            elif label == 0x036:
+                self.parse_block_unk36(subblock)
+            elif label == 0x070:
+                self.parse_block_user_info(subblock)
+            elif label == 0x100:
+                self.parse_block_unk100(subblock)
+            else:
+                print("Unknown block {}".format(hex(label)))
+
+    def parse_block_mesh(self, data):
+        a = 0
+    def parse_block_unk30(self, data):
+        a = 0
+    def parse_block_unk31(self, data):
+        a = 0
+    def parse_block_vertices(self, data):
+        a = 0
+    def parse_block_faces(self, data):
+        a = 0
+    def parse_block_properties(self, data):
+        a = 0
+    def parse_block_unk35(self, data):
+        a = 0
+    def parse_block_unk36(self, data):
+        a = 0
+    def parse_block_user_info(self, data):
+        a = 0
+    def parse_block_unk100(self, data):
+        a = 0
 
 class BESImporter(bpy.types.Operator, ImportHelper):
     bl_idname = "import_mesh.bes"
